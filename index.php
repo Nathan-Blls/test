5064d20274323ad009774d459407e4ddfc33b693
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-25 12:26:02 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
-<?php
-
-echo "Hello World en PHP !";
-
-?>
-
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+        <?= "Accueil";?>
+    </title>
+</head>
+<body>
+    <h1>
+        <?php echo "Accueil" ?>
+    </h1>
+</body>
+</html>
