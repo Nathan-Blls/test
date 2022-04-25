6e2cb5593f48e38acf2d4ca451c9a98addf2f51c
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-25 15:41:01 +0200

    Opérations arithmétiques

--- test.php
+++ test.php
@@ -1,10 +1,41 @@
+Opérations arithmétiques :
+<br>
+<br>
 <?php
-echo "titi <br>";
 
+$a = 2;
+$b = 1;
 
-echo "tati <br>";
+echo $a + $b;
+echo "<br>";
+echo $b - $a ;
+echo "<br>";
+echo $a+=3;
+echo "<br>";
+echo $b-=1;
+echo "<br>";
+echo $a*2;
+echo "<br>";
+echo $a + $b ;
+echo "<br>";
+echo $b=+4;
+echo "<br>";
+echo $a + $b ;
+echo "<br>";
+echo ($a + $b)*$a;
 
+echo "<br>";
+echo "<br>";
 
-echo "toto";
+$nb = 5;
+$nb+=10; //15
+$nb+=15; //30
 
-echo "tonton";
+// echo $nb;
+
+$text= "Ceci est un texte";
+
+$mot = "gros mot";
+
+echo $text ." ".$mot;
+?>
