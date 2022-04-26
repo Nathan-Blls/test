b967bd768795a463bd6bd253ef1539817ec0f82c
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-26 17:08:39 +0200

    require / include etc

--- test.php
+++ test.php
@@ -1,1 +1,22 @@
 <?php
+echo "<p> Fichier Test.php </p>";
+
+include("part1.php");
+
+echo "<p>Toujours test.php</p>";
+
+include("part2.php");
+
+require("part1.php");
+
+echo "<p>Toujours test.php</p>";
+
+include "part2.php";
+
+echo "<p> Fichier test</p>";
+
+require("part1.php");
+require_once "part1.php";
+
+include_once("part2.php");
+include "part2.php";
