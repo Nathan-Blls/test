150b3771b1cb1868a789e37e7fcf727c6fe17f34
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-26 10:43:45 +0200

    ARRAY SIMPLES

--- test.php
+++ test.php
@@ -1,5 +1,5 @@
 <?php
-// LES ARRAY 
+// LES ARRAY SIMPLES
 
 $fruits = ["fraise","grenade","melon","fakya","abricot"];
 
@@ -8,6 +8,18 @@ $fruits = ["fraise","grenade","melon","fakya","abricot"];
 // var_dump : pour débugguer
 var_dump($fruits);
 
+echo "<pre>"; // <pre></pre> : pour rendre le visuel plus beau
 print_r($fruits);
+echo "</pre>";
 
 print_r("Hello");
+
+echo "<br>"; // saut de ligne
+
+echo $fruits [4]; // 4ème élément du tableau = abricot (tableau commence à 0)
+
+echo "<br>";
+
+echo $fruits [4]= "pêche"; // modification du 4ème élément par un autre.
+
+var_dump($fruits); // vérification du changement
