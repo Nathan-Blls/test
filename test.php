d98eb7df49baef786630bdbada57b47c0b2058f0
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-26 10:18:42 +0200

    delete all + test ARRAYS

--- test.php
+++ test.php
@@ -1,70 +1,13 @@
-Opérations arithmétiques :
-<br>
-<br>
 <?php
+// LES ARRAY 
 
-$a = 2;
-$b = 1;
+$fruits = ["fraise","grenade","melon","fakya","abricot"];
 
-echo $a + $b;
-echo "<br>";
-echo $b - $a ;
-echo "<br>";
-echo $a+=3;
-echo "<br>";
-echo $b-=1;
-echo "<br>";
-echo $a*2;
-echo "<br>";
-echo $a + $b ;
-echo "<br>";
-echo $b=+4;
-echo "<br>";
-echo $a + $b ;
-echo "<br>";
-echo ($a + $b)*$a;
+// echo $fruits : bug car echo pour "string"
 
-echo "<br>";
-echo "<br>";
+// var_dump : pour débugguer
+var_dump($fruits);
 
-$nb = 5;
-$nb+=10; //15
-$nb+=15; //30
+print_r($fruits);
 
-// echo $nb;
-
-$text= "Ceci est un texte";
-
-$mot = "gros mot";
-
-
-echo "<br>";
-
-$userName = "Bob";
-$a = 4;
-$b = 3;
-$c = "<br>";
-
-echo "Bonjour, je m'appelle "; echo $userName; echo " !";
-echo $c;
-echo "J'ai "; echo $b; echo $a; echo " ans !";
-echo $c;
-echo "J'habite au "; echo $b; echo ($a+$b); echo " boulevard National";
-
-echo "<br>";
-echo "<br>";
-
-// Concaténation :
-
-echo "Bonjour, je m'appelle ". $userName . " !" . $c . "J'ai " . $b.$a . " ans !" . $c . "J'habite au " .$b . 7 . " boulevard national";
-echo "<br>";
-echo "<br>";
-// Constantes :
-define ("douze", 12);
-echo douze;
-echo "<br>";
-const verite = true;
-echo verite;
-
-?>
-
+print_r("Hello");
