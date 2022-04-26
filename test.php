f063c12a586e0415b74a9dc37b17554a071b6282
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-26 12:48:12 +0200

    Tableaux associatifs / asso + dimensionnels

--- test.php
+++ test.php
@@ -14,7 +14,7 @@
 
 // 3D 
 
-$toto = "Michel";
+// $toto = "Michel";
 
 // $games = [ 
 //     ["tarkov", 10, false, ["1v1", "2v2", "3v3"]],
@@ -25,19 +25,40 @@ $toto = "Michel";
 
 // var_dump($games[0][3][1]); // = 2v2 car 0 est 1er élément du tableau, 3 le 4ème (autre tableau), et 1 "2v2"
 
-$games [] = ["toto", "tata"];
-//$games = "tata";
-var_dump($games);
+// $games [] = ["toto", "tata"];
+// //$games = "tata";
+// var_dump($games);
 
-$tab = ["a", 1, 2, true];
+// $tab = ["a", 1, 2, true];
 
-$tab[] = false; //ajout à la fin du tableau
+// $tab[] = false; //ajout à la fin du tableau
 
-var_dump($tab); // a, 1, 2, true, FALSE
+// var_dump($tab); // a, 1, 2, true, FALSE
 
-$games3D = [
-    ["tarkov", 10, false],
-    ["cod", 100, false], 
-    ["mario", 1, false], 
-    ["cs", 10, true, ["MM", "BOMBE","OTAGES"]]
+// $games3D = [
+//     ["tarkov", 10, false],
+//     ["cod", 100, false], 
+//     ["mario", 1, false], 
+//     ["cs", 10, true, ["MM", "BOMBE","OTAGES"]]
+// ];
+
+// TABLEAU ASSOCIATIF : clé et valeur
+
+// $frameWorks = [
+//     ["name" => "Angular", "dev" => "Front", "async" => true],
+//     ["name" => "Symfony", "dev" => "Full-Stack", "async" => false],
+//     ["name" => "Spring Boot", "dev" => "Full-Stack", "async" => false]
+// ];
+
+// echo $frameWorks[1]["name"]; // ici je choisis la ligne du milieu (1) et le nom = Symfony
+
+// TABLEAU ASSO + MULTI DIMENSION :
+
+$valo = [
+    "titre" => "Valorant",
+    "nbPlayers" => 10,
+    "eSport" => true,
+    "mode" => ["INTENSIFICATION","MATCHMAKING"]
 ];
+
+echo $valo["mode"][1]; // ici je choisis la clé "mode" avec la 2ème valeur (1) = MATCHMAKING 
