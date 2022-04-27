26aeb9e9d0f4ffddfc875dccc025ca28f99b22b1
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-04-27 17:42:02 +0200

    BOUCLES WHILE FOR

--- repoData.php
+++
@@ -1,9 +0,0 @@
-<?php
-$home = " 😎 Hello World 😎";
-
-$valo = [
-    "title" => "Valorant",
-    "editor" => "Riot",
-    "category" => "FPS",
-    "nb_players" => 10
-];
--- test.php
+++ test.php
@@ -1,33 +1,42 @@
-<?php require_once "repoData.php" ?> 
-<!-- ici on appelle le fichier repoData qui contient les données -->
+<?php
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>TEST</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-</head>
+// BOUCLES
 
-<body>
-    
-<div class="container">
-    <h1 class="my-5 text-center text-danger">
-<?php echo $home ?>
-<!-- ici, pour le h1, on récupère les données de repoData en appelant le nom de la variable -->
-    </h1>
+$debut = 0;
 
-    <div class="card shadow col-5 text-center">
-<h2 class="53">Titre : <?php echo $valo['title'] ?></h2>
-<p>Editeur : <?php echo $valo['editor'] ?></p>
-<p>Catégorie : <?php echo $valo['category'] ?></p>
-<p>Joueurs : <?php echo $valo['nb_players'] ?></p>
-<!-- ici, pour remplir les champs, on récupère les données de repoData en appelant le nom de la variable et sa valeur -->
-</div>
+while ($debut < 5) {
+    echo "<p>Cool !</p>";
+    $debut++;
+}
 
-</div>
+$i = 0;
 
+while($i <= 9){
+    echo "<p>Cool ! $i </p>";
+    $i++;
+}
 
-</body>
+$j = 5;
+
+while($j < 9){
+    echo "Cool ! $j";
+    $j--;
+}
+
+// BOUCLES WHILE et TABLEAU
+
+$users = ["Jean","Marie","Mohamed","John"];
+
+echo "$users[0] $users[1] $users[2] $users[3]";
+
+$a = 0;
+
+while ($a < count($users)) {
+    echo "$users [$a] <br>";
+    $a++;
+}
+// BOUCLE FOR
+
+for ($i=0; $i < 10; $i++) {
+    echo "Youpi $i <br>";
+}
