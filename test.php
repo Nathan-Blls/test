b54b591176bef28d30491d7ce47578038332524c
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-05-03 17:14:04 +0200

    fonctions

--- test.php
+++ test.php
@@ -1,32 +1,50 @@
 <?php
 
-// SWITCH CASE
+// FONCTIONS
 
-// $i = 0;
-
-// if ($i ==0) {
-//     echo "i égal 0";
-// }elseif ($i == 1) {
-//     echo "i égal 1";
-// }elseif ($i == 2) {
-//     echo "i égal 2";
-// }
-
-$i = 0;
-
-switch ($i) {
-        case 0:   // si i = 0 >> va afficher "i égal 0"
-        echo "i égal 0";
-        break;
-
-            case 1:
-            echo "i égal 1";
-            break;
-
-                case 2:
-                echo "i égal 2";
-                break;
-
-                    default:
-                    echo "i n'est ni égal à 2, ni à 1, ni à 0.";
+function lanceRoquette (){    // on créé la fonction
+    echo "La violence c'est mal";   // on lui dit quoi faire
 }
+
+// lanceRoquette();  >> affiche "La violence c'est mal"
+
+function peace (){     // la fonction paix va appeller une autre fonction
+    lanceRoquette();
+}
+
+// peace(); >> affiche "La violence c'est mal"
+// __________________
+
+function salut($name){
+    echo "<p>Hello " . $name . " my friend !</p>";
+}
+salut("Calogero");
+salut("Mickael");
+salut(12);
+
+function tourName($tour,$prenom){
+    echo "<ul>";
+    for ($i=0; $i < $tour ; $i++){
+        echo "<li> Toto $i - $prenom</li>";
+    }
+    echo "</ul>";
+}
+
+tourName(3,"Bob");
+tourName(6, "Revolver");
+
+// tourName(); bug car pas d'arguments !
+
+function multiplication($nb, $nb2){
+   return $nb * $nb2;       // renvoie le résultat, à coupler avec echo pour l'afficher
+}
+
+// // echo multiplication(2,3);   // 3*2 = 6
+
+// $resultat = multiplication(2,3);
+
+// // echo $resultat;
+
+// multiplication($resultat, multiplication(2,3));
+
+// echo $resultat;
