75d37254f08b64ad2c0bfa0bbc739e64834d1a61
Nathan-Blls <nathan.ballesteros.dev@gmail.com>
2022-05-05 16:25:02 +0200

    exos tri tableau, "lunettes", "prénoms"

--- test.php
+++ test.php
@@ -1,66 +1,69 @@
 <?php
 
-// EXERCICE FIZZBUZZ : 
+// EXERCICE LUNETTES :
 
-// FAIRE UN FIZZ BUZZ AVEC IF ELSE
-// PUIS REFAIRE AVEC SWITCH CASE
+// Faire le code qui répond aux questions "combien et qui porte des lunettes"
+// idéalement tableau associatif
+// V1 : tableau en dur
+// V2 : utiliser RANDOM pour générer lunettes ou pas
+// V3 : Faire un visuel (card) avec cadre (bordure) en rouge si pas de lunettes
+// V4 : Users RANDOM
 
-// SI MULTIPLE DE 3 - FIZZ
-// SI MULTIPLE DE 5 - BUZZ
-// SI LES DEUX - FIZZBUZZ
-// SI AUCUN JUSTE L’ITÉRATION
+//V1 alternative : strlength pour dire combien et qui ont tel nombre de lettres dans leur prénom
+$classMars = [
+  ["name" => "Florian" , "lunettes" => false],
+  ["name" => "Emmanuel" , "lunettes" => true],
+  ["name" => "Hakim" , "lunettes" => false],
+  ["name" => "Tristan" , "lunettes" => false],
+  ["name" => "Anis" , "lunettes" => false],
+  ["name" => "Laurent" , "lunettes" => false],
+  ["name" => "Anthony" , "lunettes" => true],
+  ["name" => "Nathan" , "lunettes" => false],
+  ["name" => "Kevin" , "lunettes" => false],
+  ["name" => "William" , "lunettes" => false],
+  ["name" => "Daouda" , "lunettes" => false],
+  ["name" => "Hajara" , "lunettes" => false],
+  ["name" => "Soufiane" , "lunettes" => false],
+  ["name" => "Christopher" , "lunettes" => true],
+  ["name" => "Trucmuchebidule" , "lunettes" => true],
+];
+//V1 alternative : strlength pour dire combien et qui ont tel nombre de lettres dans leur prénom
+$court = 0;
+$long = 0;
 
-// AFFICHER TOUTES LES ITÉRATIONS
-
-// BOUCLE IF ELSE SWITCH COMPARATEUR
-// PUIS POUR LES PLUS RAPIDES FAIRE UNE V2 SI VOUS
-// AVEZ DES IDÉES.
-
-// % MODULO
-// FOR WHILE
-// IF SWITCH
-// __________________
-
-// AVEC IF ET ELSE :
-
-for ($i=0; $i <= 20; $i++) {                // $i=0 commencer itération à zéro / 
-                                            // $i <= 20 : boucle tant que $i n'est pas à 20 
-                                            // $i++ incrémentation de 1 à chaque itération
-
-    if (($i % 3 == 0) && ($i % 5 == 0)) {    // si multiples de 3 et 5
-      echo "$i - FizzBuzz <br>";             // afficher Fizzbuzz et revenir à la ligne
-      } else if ($i % 3 == 0) {              // sinon, si multiples de 3
-        echo "$i - Fizz <br>";               // afficher Fizz et revenir à la ligne
-        } else if ($i % 5 == 0) {            // sinon, si multiples de 5
-          echo "$i - Buzz <br>";             // afficher buzz et revenir à la ligne
-          } else {                           // sinon, juste afficher le nombre d'itération et revenir à la ligne
-            echo "$i <br>";
+foreach ($classMars as $class) {
+        if (strlen($class["name"])<=7) {           // si le nom a - ou égal 7 lettres
+            echo ($class["name"]) . " : " . strlen($class["name"]) . " lettres (- ou = 7)" .'<br><hr> '; // montrer prenom : nbre char...
+            $court++;     // itération suivante et stocke nbre itérations du cas <=7
+          } if (strlen($class["name"])>=8) {         // si le nom a + ou égal 8 lettres
+            echo ($class["name"]) . " : " . strlen($class["name"]) . " lettres (+ ou = 8)" .'<br><hr> ';   // montrer prenom : nbre char...
+            $long++;       // itération suivante et stocke nbre itérations du cas <=7
           }
-        }
+      }
+    echo "<br>";
+    echo "$court personnes ont un prénom de 7 lettres ou moins. <br>";
+    echo "$long personnes ont un prénom de 8 lettres ou plus. <br>";
 
-        // ________________________
-?>
+    //_________________________
 
- <br>
- <br>
+    // V1 :
 
-   <?php
-    // AVEC SWITCH CASE :
-   for ($i=0; $i <= 20; $i++) {
+// $lunettes=0;
+// $nonLunettes=0;
 
-            echo ($i . " ");               // Affiche le numéro d'itération
-            switch (true) {
-                case $i % 3 == 0 && $i % 5 == 0:    // cas où multiples de 3 et 5
-                    echo " - FIZZBUZZ";
-                    break;
+// foreach ($classMars as $class) {
+//         if ($class["lunettes"]==true) {
+//             echo $class["name"].'<br> ';
+//             $lunettes++;
+//         }
+//         if ($class["lunettes"]==false) {
+//             $nonLunettes++;
+//         }
+//     }
+//     echo "<br>";
+//     echo "$lunettes personne(s) ont des lunettes. <br>";
+//     echo "$nonLunettes personne(s) ont  pas des lunettes. <br>";
 
-                case $i % 3 == 0:    // multiples de 3
-                    echo " - FIZZ";
-                    break;
 
-                case $i % 5 == 0:    // multiples de 5
-                    echo " - BUZZ";
-                    break;
-            }
-        echo "<br><hr>";    // retour à la ligne + séparateur
-        }
+//__________________
+
